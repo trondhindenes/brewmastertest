ad3421f6fc178e4d04b906d8ad94fc3233692768
Trond Hindenes <trond@hindenes.com>
2014-11-14 18:35:29 +0100

    First commit

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using Brewmaster.TemplateSDK.Contracts.Fluent;
+using Brewmaster.TemplateSDK.Contracts.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace BrewMasterTEst1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var template = WithTemplateExtensions
+                    .CreateTemplate("Brewmaster.DemoTemplate", " This is a demo template.")
+                    .WithParameter("Region", ParameterType.String, "Name of Azure region.", "AzureRegionName")
+                    .WithParameter("VmName", ParameterType.String, "Name of the VM", "VmName")
+                    .WithCloudService("{{VmName}}", "Brewmaster ARR Cloud Service",
+                    cs => cs.WithDeployment(null, d =>
+                    d.UsingDefaultDiskStorage("{{DiskStore}}")
+                        .WithVirtualMachine("{{VmName}}", "{{Medium}}", "arr-avset", vm =>
+                                            vm.WithWindowsConfigSet("vmadmin")
+                                            .UsingConfigSet("ArrServer")))
+                );
+
+
+
+            string path = Directory.GetCurrentDirectory();
+            //string target = @"c:\temp";
+            Console.WriteLine("The current directory is {0}", path);
+
+            template.Save(path);
+        }
+    }
+}
---
+++ bin/Debug/README.md
@@ -0,0 +1,1 @@
+Brewmaster Template : Brewmaster.DemoTemplate
